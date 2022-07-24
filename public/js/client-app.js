37e3d16b18c955ddf7e942e3a62039e4b3c1b1fd
Tahir Siddiqui <tssiddiqui1993@gmail.com>
2022-07-24 18:17:43 -0500

    add frontend code for displaying forecast

--- public/js/client-app.js
+++ public/js/client-app.js
@@ -1,14 +1,34 @@
 
 console.log("Client side javascript")
 
-fetch('http://localhost:3000/weather?location=boston').then( (response) =>{
+
+
+const form =  document.querySelector('form')
+const input = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+
+
+form.addEventListener('submit', (e)=>{
+
+    e.preventDefault()
+
+    message1.textContent = "Loading..."
+    message2.textContent = ''
+
+    fetch('http://localhost:3000/weather?location='+ input.value).then( (response) =>{
+    
     response.json().then((data)=>{
 
         if(data.error){
-            console.log(error)
+            message2.textContent = data.error
         }else{
-            console.log(data)
+            message1.textContent = "it is " + data.forecast + " degrees in " + data.location
         }
     })
 
 })
+    
+})
+
